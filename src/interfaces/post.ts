89430404c935d102d3e0ff0dd727ac20767d38e1
Sumin <robolindasoo@gmail.com>
2024-04-09 12:21:18 +0900

    :label: interface 추가

---
+++ src/interfaces/post.ts
@@ -0,0 +1,15 @@
+import {Author} from "@/interfaces/author";
+
+export type Post = {
+    slug: string;
+    title: string;
+    date: string;
+    coverImage: string;
+    author: Author;
+    excerpt: string;
+    ogImage: {
+        url: string;
+    };
+    content: string;
+    preview?: boolean;
+};
