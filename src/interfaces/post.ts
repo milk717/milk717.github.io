851398332c0261d68d41bfeefc435adda3df1deb
Sumin <robolindasoo@gmail.com>
2024-04-09 14:27:08 +0900

    :fire: author 타입 제거

--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -1,12 +1,9 @@
-import {Author} from '@/interfaces/author';
-
 export type Post = {
   slug: string;
   title: string;
   date: string;
   thumbnail: string;
   tags: string[];
-  author: Author;
   excerpt: string;
   ogImage: {
     url: string;
