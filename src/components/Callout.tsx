56f46496d20f865b2a33b2937e557075a01494a1
milk717 <robolindasoo@gmail.com>
2024-04-18 10:05:34 +0900

    :arrow_up: obsidian-callouts-markdown 버전업

---
+++ src/components/Callout.tsx
@@ -0,0 +1,8 @@
+'use client';
+import {ObsidianCallout} from 'obsidian-callouts-markdown';
+
+const Callout = ({...props}) => {
+  return <ObsidianCallout {...props} />;
+};
+
+export default Callout;
