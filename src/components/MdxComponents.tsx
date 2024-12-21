a211fa255dda7f9f1a3b6e5de0d044c18921d739
Sumin <robolindasoo@gmail.com>
2024-12-21 22:37:20 +0900

    :lipstick: 가독성 개선

--- src/components/MdxComponents.tsx
+++ src/components/MdxComponents.tsx
@@ -6,35 +6,35 @@ import {useMDXComponent} from 'next-contentlayer/hooks';
 const components: MDXComponents = {
   h1: props => (
     <h1
-      className="font-bold text-3xl text-neutral-800 my-4 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
+      className="font-bold text-3xl text-neutral-700 my-8 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
       {...props}>
       {props.children}
     </h1>
   ),
   h2: props => (
     <h2
-      className="font-bold text-2xl text-neutral-800 mt-4 mb-2 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
+      className="font-bold text-2xl text-neutral-700 my-7 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
       {...props}>
       {props.children}
     </h2>
   ),
   h3: props => (
     <h3
-      className="font-bold text-xl text-neutral-800 mt-3 mb-1 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
+      className="font-bold text-xl text-neutral-700 mt-6 mb-4 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
       {...props}>
       {props.children}
     </h3>
   ),
   h4: props => (
     <h4
-      className="font-bold text-lg text-neutral-800 mt-2 mb-0.5 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
+      className="font-bold text-lg text-neutral-700 mt-2 mb-0.5 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
       {...props}>
       {props.children}
     </h4>
   ),
   h5: props => (
     <h5
-      className="font-semibold text-base text-gray-700 mt-1 mb-0.5 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
+      className="font-semibold text-base text-neutral-700 mt-1 mb-0.5 hover:after:content-['#'] after:mx-1 after:text-neutral-200"
       {...props}>
       {props.children}
     </h5>
@@ -47,30 +47,30 @@ const components: MDXComponents = {
     </h6>
   ),
   p: props => (
-    <p className="my-2 text-base text-neutral-700 leading-7" {...props}>
+    <p className="my-4 text-base text-neutral-600 leading-loose" {...props}>
       {props.children}
     </p>
   ),
   span: props => (
     <span
-      className="text-base text-neutral-700 leading-7 group-aria-[label=inline-code-block]:text-sm group-aria-[label=inline-code-block]:text-violet-800"
+      className="text-base text-neutral-700 leading-loose group-aria-[label=inline-code-block]:text-sm group-aria-[label=inline-code-block]:text-violet-800"
       {...props}>
       {props.children}
     </span>
   ),
   strong: props => (
-    <strong className="font-bold text-indigo-800 leading-relaxed" {...props}>
+    <strong className="font-bold text-indigo-800 leading-loose" {...props}>
       {props.children}
     </strong>
   ),
   a: props => (
-    <a className="text-base text-blue-700 leading-relaxed" {...props}>
+    <a className="text-base text-blue-700 leading-loose" {...props}>
       {props.children}
     </a>
   ),
   pre: props => (
     <pre
-      className="p-3 rounded-lg overflow-x-scroll bg-[#282c34] group"
+      className="p-3 rounded-lg overflow-x-scroll bg-[#282c34] group leading-loose"
       aria-label="code-block"
       {...props}>
       {props.children}
@@ -78,24 +78,24 @@ const components: MDXComponents = {
   ),
   code: props => (
     <code
-      className="text-sm font-medium leading-relaxed px-1 py-0.5 mx-0.5 rounded bg-violet-50 border border-violet-300 group group-aria-[label=code-block]:bg-transparent group-aria-[label=code-block]:border-none"
+      className="text-sm font-medium leading-loose px-1 py-0.5 mx-0.5 rounded bg-violet-50 border border-violet-300 group group-aria-[label=code-block]:bg-transparent group-aria-[label=code-block]:border-none"
       aria-label="inline-code-block"
       {...props}>
       {props.children}
     </code>
   ),
   ul: props => (
-    <ul className="leading-relaxed list-disc ms-4" {...props}>
+    <ul className="leading-loose list-disc ms-4" {...props}>
       {props.children}
     </ul>
   ),
   ol: props => (
-    <ol className="leading-relaxed list-decimal ms-5" {...props}>
+    <ol className="leading-loose list-decimal ms-5" {...props}>
       {props.children}
     </ol>
   ),
   li: props => (
-    <li className="leading-relaxed text-base text-neutral-700" {...props}>
+    <li className="leading-loose text-base text-neutral-700" {...props}>
       {props.children}
     </li>
   ),
@@ -106,7 +106,7 @@ const components: MDXComponents = {
   ),
   thead: props => (
     <thead
-      className="text-neutral-800 rounded-lg bg-gradient-to-br from-indigo-50 via-purple-50 to-blue-50"
+      className="text-neutral-700 rounded-lg bg-gradient-to-br from-indigo-50 via-purple-50 to-blue-50"
       {...props}>
       {props.children}
     </thead>
@@ -133,7 +133,7 @@ const components: MDXComponents = {
     <ObsidianCallout
       components={{
         normal: props => (
-          <div className="border-l-4 border-l-indigo-200 ps-2">
+          <div className="border-l-4 border-l-indigo-200 ps-2 leading-loose">
             {props.children}
           </div>
         ),
