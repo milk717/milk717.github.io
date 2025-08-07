b11f9d91c31ba0b00b9b0a411e4ed811d1e3b379
milk717 <robolindasoo@gmail.com>
2025-08-07 14:52:44 +0900

    ⚡️ 이미지 로드 개선

--- src/components/TagItem.tsx
+++ src/components/TagItem.tsx
@@ -1,32 +1,36 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import BlogInfo from '@/BlogInfo.json';
+import { Image } from "@/components/ui/image";
+import Link from "next/link";
+import BlogInfo from "@/BlogInfo.json";
+import { AspectRatio } from "./ui/aspect-ratio";
 
 type TagItemProps = {
-  tagName: string;
-  tagCnt: number;
+	tagName: string;
+	tagCnt: number;
 };
 
 //FIXME 타입 고치기
-const TagItem: React.FC<TagItemProps> = ({tagName, tagCnt}) => {
-  return (
-    <Link
-      key={tagName}
-      href={`/post?tag=${tagName}`}
-      className="flex flex-col items-center gap-y-2 rounded-lg bg-white border py-2 cursor-pointer hover:bg-gradient-to-br hover:from-indigo-50 hover:via-purple-50 hover:to-blue-50">
-      <Image
-        className="object-cover"
-        // @ts-ignore
-        src={BlogInfo.tagImages[tagName]}
-        alt={`${tagName}의 썸네일`}
-        width={80}
-        height={100}
-      />
-      <p className="font-semibold px-2 py-0.5 text-neutral-800">
-        {tagName} ({tagCnt})
-      </p>
-    </Link>
-  );
+const TagItem: React.FC<TagItemProps> = ({ tagName, tagCnt }) => {
+	return (
+		<Link
+			key={tagName}
+			href={`/post?tag=${tagName}`}
+			className="flex flex-col items-center gap-y-2 rounded-lg bg-white border p-4 cursor-pointer hover:bg-gradient-to-br hover:from-indigo-50 hover:via-purple-50 hover:to-blue-50"
+		>
+			<AspectRatio ratio={1}>
+				<Image
+					className="object-cover rounded-lg"
+					// @ts-ignore
+					src={BlogInfo.tagImages[tagName]}
+					alt={`${tagName}의 썸네일`}
+					fill
+				/>
+			</AspectRatio>
+
+			<p className="font-semibold px-2 py-0.5 text-neutral-800">
+				{tagName} ({tagCnt})
+			</p>
+		</Link>
+	);
 };
 
 export default TagItem;
---
+++ src/components/ui/image.tsx
@@ -0,0 +1,23 @@
+import NextImage from "next/image"
+import { cn } from "@/lib/utils"
+
+interface ImageProps extends React.ComponentProps<typeof NextImage> {
+  showSkeleton?: boolean
+}
+
+export function Image({ 
+  className, 
+  showSkeleton = true,
+  placeholder = showSkeleton ? "blur" : "empty",
+  blurDataURL = showSkeleton ? "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMSIgaGVpZ2h0PSIxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNmNGY0ZjQiLz48L3N2Zz4=" : undefined,
+  ...props 
+}: ImageProps) {
+  return (
+    <NextImage
+      {...props}
+      className={cn(className)}
+      placeholder={placeholder}
+      blurDataURL={blurDataURL}
+    />
+  )
+}
